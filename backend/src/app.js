fb8a1ffcc24926fc8638e3f8cd351e542c87cc79
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-05-07 11:59:46 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,34 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-const app = express()
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-/*
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true 
-}))
-*/
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+const app = express();
 
-import userRouter from "./routes/user.router.js"
-import videoRouter from "./routes/video.routes.js"
-import commentRouter from "./routes/comment.routes.js"
+// Middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+// Uncomment the following line if you need to configure CORS
+// app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
-import likeRouter from "./routes/like.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
+// Importing routers
+import userRouter from "./routes/user.router.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
+// Using routers
+app.use('/api/v1/users', userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.use('/api/v1/users',userRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/tweets", tweetRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-
-export {app}
-
+// Exporting the app instance
+export default app;
