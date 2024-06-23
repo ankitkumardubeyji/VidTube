98673c895e479214ed4b42e88d420b482447f74b
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-06-23 14:26:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'https://vidtube-7f2l.onrender.com',
-    }
-  },
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://vidtube-7f2l.onrender.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
